230d2e5b37a9acabdf12cd1aed51cd52db1d8b11
Michael "Cal" Krug <cal.krug@proton.me>
2022-08-02 19:16:36 -0500

    Add: init code

---
+++ src/test/java/com/tts/ttsoauthhw/TtsOauthhwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.ttsoauthhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TTSOAuthHWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
